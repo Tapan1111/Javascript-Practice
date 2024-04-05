564a0b35f49429fbe0d5e95afc016be736928b7c
Tapan Behera <107788391+Tapan1111@users.noreply.github.com>
2024-04-05 20:09:37 +0000

    today we dived into data types and their conversions

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // now all codes will be treated as newer version of javascript   
+
+let name = "Tapan behera";
+let age = 18;
+let isLoggedIn = true;
+
+//Data-types
+// number   => 2 power 53
+// bigInt
+// String => "";
+// null  => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);//undefined
+console.log(typeof undefined);//Object
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn);
+
+//  1 => true
+// "" => false
+// "tapan" => true
+
+let num = 33
+
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
+
+// **************************** Operations ******************************//
+
